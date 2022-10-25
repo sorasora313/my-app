d027cfee0285654a92718f9eb34728a43c5d161d
sorasora313 <sky.313.sora@gmail.com>
2022-10-26 07:03:54 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,28 +4,37 @@ import './index.css';
 
 
 class Square extends React.Component {
+  // TODO: remove the constructor
   constructor(props) {
     super(props);
     this.state = {
-      value: null  
+      value: null,
     };
   }
+
   render() {
+    // TODO: use onClick={this.props.onClick}
+    // TODO: replace this.state.value with this.props.value
     return (
-      <button className="square" onClick={function()
-      {console.log('click');}}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>;
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
 
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} />;
+  }
+  
   render() {
     const status = 'Next player: X';
 
@@ -33,19 +42,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
@@ -72,3 +75,4 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
